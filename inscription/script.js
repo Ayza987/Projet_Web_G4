c29408b46db8befa57edb96bf1cb96bfce73aab1
youmbidan <daniel.youmbi@2027.ucac-icam.com>
2024-03-10 23:16:17 +0100

    page d'inscription et phase 1 de la page d'accueil

--- inscription/script.js
+++ inscription/script.js
@@ -2,14 +2,16 @@ function submitForm(event) {
     event.preventDefault();
 
     // Récupérer les valeurs du formulaire
-    
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    
+    const cesiCenter = document.getElementById('cesiCenter').value;
 
     // Vous pouvez traiter les données ici, par exemple les envoyer à un serveur
-    
+    console.log('Prénom:', firstName);
+    console.log('Nom:', lastName);
     console.log('E-mail:', email);
     console.log('Mot de passe:', password);
-    
+    console.log('Centre CESI:', cesiCenter);
 }
